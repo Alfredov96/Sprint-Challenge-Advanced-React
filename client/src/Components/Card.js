f0b996bd510c75df46869829222e686bc00527c3
alfredo villasenor <alfredo.villasenor.aof@gmail.com>
2020-04-10 10:59:21 -0700

    app almost done and card almost made fixing some bugs

---
+++ client/src/Components/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Card (props) {
+    return (
+        <div>
+            {props.cardData.map(stuff => {
+                <div key={stuff.id}>
+
+                    <h2>{stuff.name}</h2>
+                    <h3>{stuff.country}</h3>
+
+                </div>
+            })}
+        </div>
+    )
+}
