290654df39a9026c41bebc7a262f4975b397d1cf
alfredo villasenor <alfredo.villasenor.aof@gmail.com>
2020-04-10 14:36:21 -0700

    finished

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import '@testing-library/jest-dom/extend-expect'
+import Card from './Components/Card';
+import NavBar from './Components/NavBar';
+import '@testing-library/jest-dom/extend-expect';
 import { render, fireEvent, waitFor, screen } from '@testing-library/react'
 
 
@@ -9,4 +11,16 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders NavBar.js without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<NavBar />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+test('Current App Header is Displaying', () => {
+  const { getByText } = render(<App />);
+  const headerText = getByText(/look at them champs!/i);
+  expect(headerText).toBeVisible();
 });
